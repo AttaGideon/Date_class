ab487c0b64c05309d45f457a5666359a340d8bc6
Gideon Appiagyei Atta <78235843+AttaGideon@users.noreply.github.com>
2022-11-11 16:37:50 +0100

    Create TestDate.cpp
    inital commit - testDate.cpp

---
+++ TestDate.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : TestDate.cpp
+// Author      : Gideon Atta
+// Description : Date class 
+// License     : Free to use and Modify
+//============================================================================
+#include "Date.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "!!!Date, Looking Good!!!";
+	cout << "\n\n";
+
+	return 0;
+}
