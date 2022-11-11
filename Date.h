424a6442119fc0e2ae258aa369186eecc3c1dc59
Gideon Appiagyei Atta <78235843+AttaGideon@users.noreply.github.com>
2022-11-11 16:33:00 +0100

    Create Date.h
    Initial .h file commit

---
+++ Date.h
@@ -0,0 +1,30 @@
+#ifndef DATE_HPP
+#define DATE_HPP
+#include <iostream>
+#include <sstream>
+#include <string>
+
+class Date
+{
+public:
+	Date();  // Default constructor
+	Date(int day, int month, int year); // main constructor
+	Date(const Date &otherDate);  // copy constructor
+	~Date(); //Detructor
+
+	void DisplayDate(); // To display the date.
+	bool CheckMonth(int day, int month, int year);  // To check if the entered day of the month is correct.
+	bool setDate(int day, int month, int year);  // Check if the entered date is correct and set the  new date
+	bool checkDate(int day, int month, int year);  // Check if the entered date is correct
+	bool isBefore(Date &otherDate);  // defines which date comes first
+	Date addDay();  // add a day to the correct date
+
+private:
+	int _day;
+	int _month;
+	int _year;
+	int _dayCount = 0;
+	std::string DateToString;
+  
+};
+#endif
