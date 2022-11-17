4e2b9cd1ce291dee82cb80ef6a15766df1309a9c
Gideon Appiagyei Atta <78235843+AttaGideon@users.noreply.github.com>
2022-11-17 10:34:44 +0100

    Update TestDate.cpp
    added different test cases

--- TestDate.cpp
+++ TestDate.cpp
@@ -10,8 +10,38 @@
 using namespace std;
 
 int main() {
+	
 	cout << "!!!Date, Looking Good!!!";
 	cout << "\n\n";
+
+	Date epoch_date = Date();
+	cout << "Time-tracking in computing is done as a count since: " << epoch_date << endl;
+
+	Date today(11,11,2022);
+	cout << "\nToday is: \n\t\t" << today << endl;
+
+	Date today1 = today;
+	Date tomorrow = today1.addDay();
+	cout << "\nTomorrow is: \n\t\t" << tomorrow <<endl << endl;
+
+	if(today.isBefore(tomorrow))
+		cout << today << " is before " << tomorrow << endl <<endl;
+	else
+		cout << tomorrow << " is after " << today << endl;
+
+	Date setAnotherDate;
+	setAnotherDate.setDate(31, 12, 2022);
+	cout << "Another set Date: \n\t\t" << setAnotherDate << endl;
+
+	Date lastDate22(31,12,2022);
+	if((lastDate22.checkDate(31, 12, 2022)))
+	{
+		cout << "\n2022 last day checked. It's Saturday:\n";
+		cout << "\t\t\t\t\t" << lastDate22 << endl;
+	}
+	else
+		cout << "Invalid Date!!!" << endl;
+
 
 	return 0;
 }
