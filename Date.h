91907f742766713900914fbc41e285749de52111
Gideon Appiagyei Atta <78235843+AttaGideon@users.noreply.github.com>
2022-11-17 10:26:30 +0100

    Update Date.h
    added overloaded operators methods

--- Date.h
+++ Date.h
@@ -19,9 +19,65 @@ public:
 	bool isBefore(Date &otherDate);  // defines which date comes first
 	Date addDay();  // add a day to the correct date
 	
-	// TODO: add operators if required for the test cases
-	/* <, > ==, ...*/
+	// Required (overloaded) operators
+	operator const char *()
+	{
+		std::ostringstream formattedDate;
+		formattedDate << _day << "." << _month << "." << _year;
 
+		DateToString = formattedDate.str();
+		return DateToString.c_str();
+	}
+	Date operator --()
+	{
+		--_day;
+		return *this;
+	}
+	Date operator ++()
+	{
+		++_day;
+		return *this;
+	}
+	Date operator = (const Date &otherDate)
+	{
+		// std::cout << "Creating Deep Copy" << std::endl;
+		_day = otherDate._day;
+		_month = otherDate._month;
+		_year = otherDate._year;
+		return *this;
+	}
+	bool operator == (const Date& otherDate) const
+	{
+		return ((_day == otherDate._day)
+				&& (_month == otherDate._month)
+				&& (_year == otherDate._year));
+	}
+	bool operator < (const Date &otherDate)
+	{
+		if(_year < otherDate._year)
+			return true;
+
+		else if((_year == otherDate._year) && (_month < otherDate._month))
+			return true;
+
+		else if((_day < otherDate._day) && (_month == otherDate._month) && (_year == otherDate._year))
+			return true;
+
+		else return false;
+
+	}
+	bool operator <= (const Date &otherDate)
+	{
+		if(this->operator ==(otherDate))
+			return true;
+		else
+			return (this->operator < (otherDate));
+	}
+	bool operator > (const Date &otherDate)
+	{
+		return !(this->operator <= (otherDate));
+	}
+	
 private:
 	int _day;
 	int _month;
